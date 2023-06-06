e8098bb938ae016fcddda6316d731e77b8165a4a
afsharm <afshar.mohebbi@gmail.com>
2023-06-06 16:22:23 +0330

    get users

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { User } from './user.entity';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('users')
+  public getUser(): Promise<User[]> {
+    return this.appService.getUsers();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
 export class AppService {
+  @InjectRepository(User)
+  private readonly repository: Repository<User>;
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  public getUsers(): Promise<User[]> {
+    return this.repository.find();
   }
 }
